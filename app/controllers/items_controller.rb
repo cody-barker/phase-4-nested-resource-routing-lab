8d7998370e4e087bdde78b664f6b52ef12a86b72
Cody <codybarker.or@gmail.com>
2023-06-14 10:14:43 -0700

    s

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  # /users/:user_id/items
+  def create
+    user = User.find(params[:user_id])
+    new_item = Item.create(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price]
+    )
+    user.items << new_item
+    render json: user, include: :items, status: :created
+    new_item
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
   end
 
 end
